4001b8c50a937b7fcc8c22b6e7788ea1b7f91304
EarlSeandrikCinco <earlseandrikcinco@gmail.com>
2024-08-19 18:58:31 +0800

    Exercise1 final version

---
+++ src/prelim/Exercise1.java
@@ -0,0 +1,23 @@
+/*
+ *Author: ( Cinco, Earl Seandrik, Campolet )
+ *Programming Date : 19/08/2024
+ *Activity Name and Number: Prelim Exercise Number 1
+ */
+//This source code should be saved in a file called Exercise1.java
+package prelim;
+import java.lang.*; // import statement
+public class Exercise1 { // class declaration
+    public static void main(String[] args) { // main method declaration
+        System.out.println("*******************************************");
+        System.out.println("*                                         *");
+        System.out.println("*   Earl Seandrik C. Cinco                *");
+        System.out.println("*    •First Year BSCS Student             *");
+        System.out.println("*    •Saint Louis University              *");
+        System.out.println("*                                         *");
+        System.out.println("*    Phone: 09626567719                   *");
+        System.out.println("*    Email: earlseandrikcinco@gmail.com   *");
+        System.out.println("*                                         *");
+        System.out.println("*******************************************");
+        System.exit(0); // terminate the program
+    } // end of main method
+} // end of Exercise1 class
